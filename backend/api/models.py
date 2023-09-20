29d8b546764bbc15ad20534891835e5a1a66c224
Bewin007 <bewinfelix@karunya.edu.in>
2023-09-20 12:26:46 +0530

    added some models

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,3 +2,104 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=40)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    profile_pic = models.ImageField(upload_to='profile_pic/')
+    usertype = models.CharField(max_length=10)
+    bank_details = models.ForeignKey(Bank_Details, on_delete=models.SET_NULL, null=True, blank=True)
+    join_date = models.DateField(auto_now_add=True)
+    last_login_timestamp = models.DateTimeField(null=True, blank=True)
+    dob = models.DateField()
+
+class Expert(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=40)
+    expert_bio = models.CharField(max_length=400)
+    expert_location = models.CharField(max_length=100)
+    expert_experience = models.IntegerField()
+    expert_rating = models.IntegerField()
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+class Gig(models.Model):
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    show_case_image = models.ImageField(upload_to='Gig/')
+    description = models.CharField(max_length=300)
+
+class Review(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True) ##to note
+    expert = models.ForeignKey(Expert, on_delete=models.SET_NULL, null=True, blank=True)
+    star_rating = models.IntegerField()
+    review_text = models.CharField(max_length=2000)
+    review_timestamp = models.DateField(auto_now_add=True)
+
+
+class FAQ(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    question =models.CharField(max_length= 150)
+    answer = models.CharField(max_length=1000)
+
+class Expert(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.SET_NULL, null=True, blank=True)
+    expert = models.ForeignKey(Expert, on_delete=models.SET_NULL, null=True, blank=True)
+    reply = models.CharField(max_length=1000)
+    reply_timestamp = models.DateField(auto_now_add=True)
+
+class Gig_package(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE) 
+    package_type = models.CharField(max_length=10)
+    package_description = models.TextField()
+    duration = models.TimeField()
+    file_attachment = models.BooleanField()
+
+class Order(models.Model):
+    client = models.ForeignKey()
+    expert = models.ForeignKey(Expert, on_delete=models.SET_NULL, null=True, blank=True)
+    gig = models.ForeignKey(Gig, on_delete=models.SET_NULL, null=True, blank=True)
+    gig_package = models.ForeignKey(Gig_package, on_delete=models.SET_NULL, null=True, blank=True)
+    # paymentlink 
+    ordder_date = models.DateField(auto_now_add=True)
+    order_Status = models.CharField(max_length=15)
+    # meeting_date_time =
+    # previous_meeting_date_time
+    # meeting_update_timestamp
+    # payment_type
+
+
+class Expert_Catogery(models.Model):
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Bank_Details(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_no = models.IntegerField(Gig, on_delete=models.CASCADE)
+    bank_name = models.CharField(max_length=50)
+    account_holder_name = models.CharField(max_length=100)
+    ifsc_code = models.CharField(max_length=15)
+
+class Wishlist(models.Model):
+    client = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+
+class Call_Details(models.Model):
+    client = models.ForeignKey( on_delete=models.CASCADE)###to note
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
+    call_type = models.CharField(max_length=15)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    private = models.BooleanField()
+    meet_image = models.ImageField(upload_to='proof/')
+    meeting_text = models.TextField()
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField()
+    status = models.BooleanField()
